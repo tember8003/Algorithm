3805996b0b9ac98fb062997a9f7141f7721ad97f
tember8003 <104021663+tember8003@users.noreply.github.com>
2023-08-14 01:02:46 +0900

    [Gold V] Title: 퇴사 2, Time: 232 ms, Memory: 37176 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+long long dp[1500003];
+long long day[1500003];
+long long money[1500003];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> day[i] >> money[i];
+	}
+	int final;
+	for (int i = n; i > 0; i--) {
+		final = i + day[i];
+		if (final > n + 1) {
+			dp[i] = dp[i + 1];
+		}
+		else {
+			dp[i] = max(dp[i + 1], dp[final] + money[i]);
+		}
+	}
+	cout << dp[1] << "\n";
+}
