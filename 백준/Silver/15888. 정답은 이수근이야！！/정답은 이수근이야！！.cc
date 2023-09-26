0cb5e9fecef7cb57b4279588d093ad25e57f1f1f
tember8003 <104021663+tember8003@users.noreply.github.com>
2023-09-26 10:58:56 +0900

    [Silver II] Title: 정답은 이수근이야!!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15888. 정답은 이수근이야！！/정답은 이수근이야！！.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+int cnt[6] = {2, 4, 8, 16, 32, 64};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int a, b, c;
+    int count = 0; int two_cnt = 0;
+    cin >> a >> b >> c;
+    for (int x = -100; x <= 100; x++) {
+        if (a * x * x + b * x + c == 0) {
+            count++;
+            for (int i = 0; i < 6; i++) {
+                if (cnt[i] == x) {
+                    two_cnt++;
+                }
+            }
+        }
+    }
+    if (count != 2) {
+        cout << "둘다틀렸근" << "\n";
+    }
+    else if (two_cnt == 2) {
+        cout << "이수근" << "\n";
+    }
+    else {
+        cout << "정수근" << "\n";
+    }
+    return 0;
+}
