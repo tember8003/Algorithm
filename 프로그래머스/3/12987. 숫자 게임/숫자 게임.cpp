49c97b73b438909d88ad85e4306a6fd350b8a751
tember8003 <104021663+tember8003@users.noreply.github.com>
2023-12-31 21:07:11 +0900

    [level 3] Title: 숫자 게임, Time: 21.08 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -1,18 +1,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> A, vector<int> B) {
-    sort(A.begin(),A.end());
-    sort(B.begin(),B.end());
-    int answer = 0;
-    int cnt=0;
-    for(int i=0; i<B.size(); i++){
-        if(A[cnt]<B[i]){
-            cnt++;
-            answer++;
+    int answer=0;
+    priority_queue<int,vector<int>,greater<int>> a_temp;
+    priority_queue<int,vector<int>,greater<int>> b_temp;
+    for(int i=0; i<A.size(); i++){
+        a_temp.push(A[i]);
+        b_temp.push(B[i]);
+    }
+    while(!b_temp.empty()){
+        int num_a = a_temp.top();
+        a_temp.pop();
+        while(!b_temp.empty()){
+            int num_b=b_temp.top();
+            b_temp.pop();
+            if(num_a < num_b){
+                answer++;
+                break;
+            }
         }
     }
     return answer;
