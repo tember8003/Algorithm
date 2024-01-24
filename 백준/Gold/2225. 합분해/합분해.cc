04590a5fea044f4f1002fc1a8414bbe4b088206b
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-01-24 20:19:26 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2336 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+long long dp[201][202];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j == 1 || i==0) {
+				dp[i][j] = 1;
+			}
+			else {
+				dp[i][j] += (dp[i - 1][j] + dp[i][j - 1])%1000000000;
+			}
+		}
+	}
+	cout << dp[n][k] << "\n";
+}
