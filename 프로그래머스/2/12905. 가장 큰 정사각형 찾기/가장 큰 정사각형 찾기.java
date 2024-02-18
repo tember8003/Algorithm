d8a5654f2651c53ede9bb22bd14e2cd954e08d24
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-02-18 19:12:00 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 12.41 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int answer=0;
+        if(board.length <2 || board[0].length<2){
+            return 1;
+        }
+        for(int i=1; i<board.length; i++){
+            for(int j=1; j<board[0].length; j++){
+                if(board[i][j]==0){
+                    continue;
+                }
+                board[i][j]=Math.min(board[i-1][j-1],Math.min(board[i-1][j],board[i][j-1]))+1;
+                if(answer < board[i][j]){
+                    answer=board[i][j];
+                }
+            }
+        }
+
+        return answer*answer;
+    }
+}
