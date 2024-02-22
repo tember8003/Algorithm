0cfd70d74c4df50f916db71a8e4ba2f6cffc6ca9
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-02-22 09:48:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,25 +1,6 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int[] rank = {6,6,5,4,3,2,1};
-        int zero_count=0; //lottos 배열 속 0 개수
-        int count=0; //똑같은 숫자 개수
-        for(int i=0;i<6; i++){
-            if(lottos[i]==0){
-                zero_count++;
-                continue;
-            }
-            for(int j=0; j<6; j++){
-                if(lottos[i]==win_nums[j]){
-                    count++;
-                    break;
-                }
-            }
-        }
-        
-        answer[0]=rank[count+zero_count];
-        answer[1]=rank[count];
-        
+        int[] answer = {};
         return answer;
     }
 }
