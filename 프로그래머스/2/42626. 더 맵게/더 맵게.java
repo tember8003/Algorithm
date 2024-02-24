d531a75d7753be33919fa8a80390e029883662e7
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-02-24 20:50:20 +0900

    Update 더 맵게.java

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -6,12 +6,14 @@ class Solution {
         for(int i=0; i<scoville.length; i++){
             pq.add(scoville[i]);
         }
-        while(pq.peek() < K){
-            if(pq.size() ==1){
+        while(pq.peek() < K){ //스코빌 지수 확인
+            
+            if(pq.size() ==1){ //모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1
                 return -1;
             }
-            int first=pq.poll();
-            int second= pq.poll();
+            
+            int first=pq.poll(); //가장 작은 수
+            int second= pq.poll(); //두번째로 작은 수
             pq.add(first+second*2);
             answer++;
         }
