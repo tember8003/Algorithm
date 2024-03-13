eeaba0dca069edfc2a5a39937db8c6887b994f3c
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-03-13 11:30:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 49.33 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+        for(int i=1; i<s.length(); i++){
+            if(stack.size()>0&&stack.peek()==s.charAt(i)){
+                stack.pop();
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        if(stack.isEmpty()){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
