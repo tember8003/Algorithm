e173e52257a6f114678fb262e0f03ccac0c48c79
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-05-30 14:15:36 +0900

    Update N과 M （8）.java

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -6,10 +6,9 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	//DFS 알고리즘 사용
 	static int N,M;
 	static int[] array; //배열
-	static int[] temp; //DFS 중 숫자를 넣기 위한 임시 배열
+	static int[] temp; //repeat 함수 실행 중 숫자를 넣기 위한 임시 배열
 	static StringBuilder sb = new StringBuilder(); //출력용
 	
 		public static void main(String[] args) throws IOException{
@@ -20,7 +19,7 @@ public class Main {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			array = new int[N];
-			temp = new int[M]; //DFS에서 숫자를 넣어줄 임시 배열
+			temp = new int[M]; //repeat 함수에서 숫자를 넣어줄 임시 배열
 			
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<N; i++) { //배열 입력
@@ -28,13 +27,13 @@ public class Main {
 			}
 			
 			Arrays.sort(array);//비내림차순으로 정렬
-			dfs(0,0);//DFS 실행
+			repeat(0,0);//repeat 실행
 			
 			System.out.println(sb.toString()); //결과 출력
 			
 		}
 		
-		public static void dfs(int num, int cnt) {
+		public static void repeat(int num, int cnt) {
 			if(cnt==M) { //길이가 M이 되었을 경우
 				//출력용 StringBuilder에 내용 저장
 				for(int i=0; i<M; i++) {
@@ -46,7 +45,7 @@ public class Main {
 
 			for(int i=num; i<N; i++) {//연산 식
 				temp[cnt]=array[i];
-				dfs(i,cnt+1);
+				repeat(i,cnt+1);
 			}
 		}
 	}
