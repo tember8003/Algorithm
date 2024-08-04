0c2294c65adb5d72616b79fc82049f337429cd04
tember8003 <104021663+tember8003@users.noreply.github.com>
2024-08-05 05:28:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53744 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	//dp - 배낭 문제
+	static int N,K;
+	static int[][] item;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		item = new int[N+1][2];
+		dp = new int[N+1][K+1];
+		
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			item[i][0]=Integer.parseInt(st.nextToken());
+			item[i][1]=Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=K; j++) {
+				if(item[i][0] > j) {
+					dp[i][j]= dp[i-1][j];
+				}
+				else {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-item[i][0]]+item[i][1]);
+				}
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+	}
+}
