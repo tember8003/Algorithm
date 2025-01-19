a1024e60e138eeb40b1f8ebe608c3747b8840a6d
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-01-20 03:31:01 +0900

    [Silver III] Title: queuestack, Time: 36 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.cc
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -1,43 +1,48 @@
 #include <iostream>
-#include <string>
-#include <map>
-#include <cmath>
-#include <stack>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
-bool check[100001];
+
+queue<int> queue_storage;      // 큐 역할을 하는 변수
+bool is_stack[100000];         // 데이터가 스택에 들어가야 하는지 여부를 나타내는 배열
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	queue<int> q;
-	deque<int> dq;
-	int num;
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		if (num == 0) { //0 큐 1 스택
-			check[i] = false;
-		}
-		else {
-			check[i] = true;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		if (check[i] == false) {
-			dq.push_back(num);
-		}
-	}
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		cin >> num;
-		dq.push_front(num);
-		cout << dq.back() << " ";
-		dq.pop_back();
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;                     // 기존 데이터의 개수
+    int m;                     // 새로 삽입할 데이터의 개수
+    int value[100001] = {};                 // 입력값
+    cin >> n;
+
+    // 데이터가 스택에 들어가야 하는지 여부 입력
+    for (int i = 0; i < n; i++) {
+        cin >> is_stack[i];
+    }
+
+    // 기존 데이터 입력
+    for (int i = 0; i < n; i++) {
+        cin >> value[i];
+    }
+
+    //데이터 처리
+    for (int i = n-1; i >= 0; i--) { //역순으로 입력받은 데이터를 처리해야 함!!
+        if (!is_stack[i]) {  // 0이라면 값 넣어주기 
+            queue_storage.push(value[i]);
+        }
+    }
+
+    // 새 데이터 입력 +  결과 출력
+    int num;
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> num;
+        queue_storage.push(num);
+        cout << queue_storage.front() << " ";
+        queue_storage.pop();
+    }
+
+    return 0;
 }
