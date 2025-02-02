b3222b4d90321508ee179f3a58c3bc3f521558fd
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-02-03 05:57:44 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -1,39 +1,33 @@
 #include <iostream>
-#include <sstream>
 #include <cstring>
-#include <map>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <algorithm>
-#include <set>
+
 using namespace std;
 
-int cnt = 0;
-
-int recursion(const char* s, int l, int r) {
-	cnt++;
-	if (l >= r)
-		return 1;
-	else if (s[l] != s[r])
-		return 0;
-	else
-		return recursion(s, l + 1, r - 1);
+int recursion(const char* s, int l, int r, int &cnt) {
+    cnt++;
+    if (l >= r) return 1;
+    if (s[l] != s[r]) return 0;
+    return recursion(s, l + 1, r - 1, cnt);
 }
 
-int isPalindrome(const char* s) {
-	return recursion(s, 0, strlen(s) - 1);
+int isPalindrome(const char* s, int &cnt) {
+    return recursion(s, 0, strlen(s) - 1, cnt);
 }
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	string str;
-	for (int i = 0; i < n; i++) {
-		cnt = 0;
-		cin >> str;
-		cout << isPalindrome(str.c_str()) << " " << cnt << "\n";
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string str;
+        cin >> str;
+        int cnt = 0;
+        cout << isPalindrome(str.c_str(), cnt) << " " << cnt << "\n";
+    }
+
+    return 0;
 }
