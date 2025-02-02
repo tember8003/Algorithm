4921f1bfa85f930445d61840e26ab0960a4fb5ba
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-02-03 06:04:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,36 +1,37 @@
 #include <iostream>
-#include <string>
-#include <map>
-#include <cmath>
-#include <stack>
-#include <queue>
-#include <algorithm>
-//https://im-gonna.tistory.com/78 재귀방식 풀이 넘나 어려운 것.
+#include <vector>
+
 using namespace std;
-int result = 0;
+
 int n, s;
-int arr[21];
-void sum(int num, int hap) {
-	if (num == n) {
-		if (hap == s) {
-			result++;
-		}
-		return;
-	}
-	sum(num + 1, hap);
-	sum(num + 1, hap + arr[num]);
+vector<int> arr;
+
+int subsetSum(int num, int hap) {
+    if (num == n) {
+        return hap == s ? 1 : 0;
+    }
+    return subsetSum(num + 1, hap) + subsetSum(num + 1, hap + arr[num]);
 }
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> s;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	sum(0, 0);
-	if (s == 0) {
-		result--;
-	}
-	cout << result << "\n";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> s;
+    arr.resize(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = subsetSum(0, 0);
+
+    if (s == 0) {
+        result--;
+    }
+
+    cout << result << "\n";
+
+    return 0;
 }
