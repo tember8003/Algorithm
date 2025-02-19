366b46f998296d9d2f990efb57b1f6b7bac539a9
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-02-20 02:05:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 44 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,29 +1,35 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include <map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-long long dp[101][100001];
-long long w[101];
-long long v[101];
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n, k;
-	cin >> n >> k;
-	for (int i = 1; i <= n; i++) {
-		cin >> w[i] >> v[i];
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (j >= w[i]) {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]]+v[i]);
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
-			}
-		}
-	}
-	cout << dp[n][k] << "\n";
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<vector<int>> item(N + 1, vector<int>(2));
+    vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+
+    for (int i = 1; i <= N; i++) {
+        cin >> item[i][0] >> item[i][1];
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= K; j++) {
+            if (item[i][0] > j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - item[i][0]] + item[i][1]);
+            }
+        }
+    }
+
+    cout << dp[N][K] << "\n";
+
+    return 0;
 }
