7fd2223f8168c4cfc86bb99abc7dfe068d0441eb
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-05-27 02:19:50 +0900

    [Platinum IV] Title: 열혈강호, Time: 1000 ms, Memory: 102228 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static ArrayList<Integer>[] list;
+	static boolean[] visited;
+	static int N,M;
+	static int[] destination;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		list = new ArrayList[N+1];
+		visited = new boolean[M+1];
+		destination = new int[M+1];
+		for(int i=0; i<=N; i++) {
+			list[i]=new ArrayList<>();
+		}
+		
+		for(int i=1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			
+			for(int j=0; j<num; j++) {
+				list[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		int count=0;
+		for(int i=1; i<=N; i++) {
+			Arrays.fill(visited, false);
+			if(dfs(i)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	
+	public static boolean dfs(int num) {
+		for(int next: list[num]) {
+			if(!visited[next]) {
+				visited[next]=true;
+				if(destination[next]==0 || dfs(destination[next])) {
+					destination[next]=num;
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+}
