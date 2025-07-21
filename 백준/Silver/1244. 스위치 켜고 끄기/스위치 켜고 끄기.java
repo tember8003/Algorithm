f0c79d930b1f62aab589100ca1c0916100d20742
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-07-21 11:22:56 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 120 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -4,52 +4,44 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,k;
-    static int[] num;
+    static int n,k; //스위치 개수 , 학생 수
+    static int[] num; //스위치 상태 표시 배열
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
+        
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
 
         num = new int[n+1];
-        num[0] = Integer.MAX_VALUE;
-        st = new StringTokenizer(br.readLine());
+        num[0] = Integer.MAX_VALUE; //스위치 상태 0번은 사용 X
+
+        //스위치 상태 넣어주기.
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             num[i] = Integer.parseInt(st.nextToken());
         }
 
+        //학생 수 입력 받기
         k = Integer.parseInt(br.readLine());
         for(int i=0; i<k; i++){
             st = new StringTokenizer(br.readLine());
+
+            //각각 성별, 스위치 번호 입력 받기
             int gender = Integer.parseInt(st.nextToken());
             int switchNum = Integer.parseInt(st.nextToken());
+
             if(gender == 1){ //남자
-                int count = switchNum;
-                while(switchNum <= n){
-                    num[switchNum] = num[switchNum] == 0 ? 1 : 0; //스위치 상태가 0이면 1로, 1이면 0 으로
-                    switchNum += count;
-                }
+                studentMan(switchNum);
             }
             else{ //여자
-                int range = Math.min(switchNum - 1, n - switchNum);
-                int count = 0;
-
-                for(int j = 0; j<=range; j++){
-                    if(num[switchNum-j] == num[switchNum+j]){
-                        count = j;
-                    }
-                    else{
-                        break;
-                    }
-                }
-
-                for(int j = switchNum - count; j <= switchNum + count; j++){
-                    num[j] = num[j] == 0 ? 1 : 0;
-                }
+                studentWoman(switchNum);
             }
         }
+
+        printSwitches();
+    }
+    public static void printSwitches(){
 
         int count = 0;
         for(int i=1; i<=n; i++){
@@ -62,5 +54,33 @@ public class Main {
         }
 
         System.out.println(sb.toString());
+    }
+
+    public static void studentWoman(int switchNum){
+
+        int range = Math.min(switchNum - 1, n - switchNum);
+        int count = 0;
+
+        for(int j = 0; j<=range; j++){
+            if(num[switchNum-j] == num[switchNum+j]){
+                count = j;
+            }
+            else{
+                break;
+            }
+        }
+
+        for(int j = switchNum - count; j <= switchNum + count; j++){
+            num[j] = num[j] == 0 ? 1 : 0;
+        }
+    }
+
+    public static void studentMan(int switchNum){
+
+        int count = switchNum;
+        while(switchNum <= n){
+            num[switchNum] = num[switchNum] == 0 ? 1 : 0; //스위치 상태가 0이면 1로, 1이면 0 으로
+            switchNum += count;
+        }
     }
 }
