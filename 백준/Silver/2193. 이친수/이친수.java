3197a26af9ea92b6556d68017f99d425dd64ce5f
tember8003 <104021663+tember8003@users.noreply.github.com>
2025-07-22 13:58:38 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,20 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
-	static long[][] dp; //맨 뒷자리가 1 혹은 0인 2차원 배열
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		dp=new long[N+1][2];
-		
-		dp[1][1]=1; dp[1][0]=0; //1,0
-		
-		for(int i=2; i<=N; i++) {
-			dp[i][0]=dp[i-1][1]+dp[i-1][0];
-			dp[i][1]=dp[i-1][0];
-		}
-		System.out.println(dp[N][0]+dp[N][1]);
-	}
+public class Main {
+    static int N;
+    static long[] dp = new long[91];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
+        initDp();
+
+        System.out.println(dp[N]);
+    }
+    public static void initDp(){
+        dp[1]=1;
+        dp[2]=1;
+
+        for(int i=3; i<=90; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+    }
 }
